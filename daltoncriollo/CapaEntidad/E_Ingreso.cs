2da17273f6e14c89aeb64a2e1a8cc1c4d347d238
kleinehassler <kleinehassler@gmail.com>
2020-07-10 00:36:45 -0500

    al 10d - de Julio DB - Forms - Reports Login Roles Ingresos

---
+++ daltoncriollo/CapaEntidad/E_Ingreso.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace CapaEntidad
+{
+    public class E_Ingreso
+    {
+        public int idedocumento { get; set; }
+        public int tipodocu { get; set; }
+        public string nombredocu { get; set; }
+        public string seriedocu { get; set; }
+        public string numdocu { get; set; }
+        public int sujeto { get; set; }
+        public DateTime fecha { get; set; }
+        public decimal subtotal { get; set; }
+        public decimal iva { get; set; }
+        public decimal total { get; set; }
+        public int bodega { get; set; }
+
+        
+    }
+}
